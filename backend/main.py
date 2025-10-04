328e60f227526baa25edc067e429b071f7d99781
adidev <adidev.m4062@gmail.com>
2025-10-04 21:19:51 +0530

    deployment

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ database.Base.metadata.create_all(bind=database.engine)
 load_dotenv()
 app = FastAPI()
 
-origins = ["http://localhost:3000"]
+origins = ["http://localhost:3000", "https://marketing-content-generator.netlify.app"]
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 try:
